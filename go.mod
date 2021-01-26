282043592f4ce1ef37fa0314fca3f5da6292e9a6
donk3y <davidtgericke@gmail.com>
2021-01-26 11:43:11 +0200

    init server

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module fiber_demo
 
 go 1.15
+
+require (
+	github.com/gofiber/fiber/v2 v2.3.3 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,45 @@
 package main
 
-func main() {
-	println("Connected")
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+// Checks if envs are set in os or .env file
+func getEnvs() (string, string) {
+	host := os.Getenv("SERVER_HOST")
+	port := os.Getenv("SERVER_PORT")
+
+	if host == "" && port == "" {
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatal("Environment variables not set")
+		}
+
+		host = os.Getenv("SERVER_HOST")
+		port = os.Getenv("SERVER_PORT")
+	}
+
+	return host, port
 }
 
+
+func main() {
+	// Set environmental variables
+	host, port := getEnvs()
+
+	// TODO Add db
+
+	app := fiber.New()
+
+  app.Get("/", func(c *fiber.Ctx) error {
+    return c.SendString("Hello, World!")
+  })
+
+	// Run Server
+	log.Fatal(app.Listen(fmt.Sprintf("%s:%s", host, port)))
+}
